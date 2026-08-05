3b9e04c297cdbfcf72a105613da600fe3720ece3
sierra <sohojang320@gmail.com>
2026-08-06 03:04:03 +0900

    Use integrated GUI entry point

--- patcher/sierra_patcher/main.py
+++ patcher/sierra_patcher/main.py
@@ -4,11 +4,11 @@ import sys
 
 # robust imports (work with/without package context)
 try:
-    from . import cli, gui
+    from . import cli, gui_web as gui
     from .flags import is_dev_mode
 except ImportError:  # frozen exe starting main.py as a script
     import sierra_patcher.cli as cli
-    import sierra_patcher.gui as gui
+    import sierra_patcher.gui_web as gui
     from sierra_patcher.flags import is_dev_mode
 
 def main(argv: list[str] | None = None) -> None:
